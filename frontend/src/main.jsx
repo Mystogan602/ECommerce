75e67a812873479d2a917a652e0514d459fbc9e2
Phạm Minh Tiến <phminhtien136@gmail.com>
2024-09-23 17:33:47 +0700

    Create Home Page components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {BrowserRouter as Router} from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import ShopContextProvider from "./context/ShopContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <Router>
-    <App />
-  </Router>,
-)
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
+  </Router>
+);
